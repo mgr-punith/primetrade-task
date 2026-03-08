b689f569fdc344efc496ca0d5e2e039616c058de
Punith Moger <mogerpunith@gmail.com>
2026-03-08 15:27:28 +0530

    deploy-1

--- primetrade-backend/app/main.py
+++ primetrade-backend/app/main.py
@@ -21,7 +21,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://primetrade-task-rho.vercel.app"
+],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
